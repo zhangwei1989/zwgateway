279a2d2e7fc017619d29c7324cd771a26495413b
zhangwei <zhangwei19890518@gmail.com>
2024-05-31 15:43:32 +0800

    init zwgateway

---
+++ src/main/java/io/github/zhangwei1989/gateway/ZwgatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.zhangwei1989.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwgatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZwgatewayApplication.class, args);
+    }
+
+}
